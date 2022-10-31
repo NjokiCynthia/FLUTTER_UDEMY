219b3d3c66b1e79385b7ec3ff97cb5d949b42e7e
njokicynthia590@gmail.com <njokicynthia590@gmail.com>
2022-10-31 13:30:05 +0300

    Quiz app

---
+++ android/app/src/main/kotlin/com/example/digital_lending_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.digital_lending_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+
+  var answerQuestion;
+
+  // ignore: non_constant_identifier_names, prefer_typing_uninitialized_variables
+  // void Funtion()? String; answerQuestion;
+
+  //final VoidCallback? answerQuestion;
+
+  void Function() {
+    // TODO: implement Function
+    answerQuestion();
+  }
+
+  Quiz({
+    required this.questions,
+    required this.answerQuestion,
+    required this.questionIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(
+          questions[questionIndex]['questionText'] as String,
+        ),
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(() => answerQuestion!(answer['score'] as String),
+              answer['text'] as String);
+        }).toList()
+      ],
+    );
+  }
+}
